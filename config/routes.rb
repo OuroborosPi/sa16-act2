73dd27c41a79d6dfe1dd1e897ad2c8486c5c5306
Mario Vera <callmejumpking@proton.me>
2024-04-19 19:53:59 -0500

    Chnaged the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/work'
-  get 'pages/contact'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'work', to: 'pages#work'
+  get 'contact', to: 'pages#contact'
+  # ... any other routes you have defined
 end
